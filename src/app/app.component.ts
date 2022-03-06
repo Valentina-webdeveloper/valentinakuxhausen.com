3cb943afc09821542608042f6adf1b5b048cca20
Valentina-developer <valentina.kuxhausen@web.de>
2022-03-06 10:09:17 +0100

    addad images, started work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'm_12_valentinakuxhausen';
+export class AppComponent implements OnInit {
+  images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;   //Rest 0, beginnt wieder bei 0
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+  }
+
+
+
+
 }
