3dfcaf71444fba02816f6a4a88245f4e586febc6
Valentina-developer <valentina.kuxhausen@web.de>
2022-03-06 18:59:13 +0100

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,36 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
-  headlines = [
-    'Hi, I am Valentina',
-    'frontend developer with angular experience',
-    'and I am eager to show you my projects'
-  ];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-
-  updateImage() {
-
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;   //Rest 0, beginnt wieder bei 0
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-
-  }
-
-
-
+export class AppComponent {
 
 }
