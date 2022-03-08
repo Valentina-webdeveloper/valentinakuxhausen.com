a9835865a426a0d5d3d492ca0e5729f698ec8b1e
Valentina-developer <valentina.kuxhausen@web.de>
2022-03-08 07:41:39 +0100

    data-protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
-import { Component, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 
 
 
 
 
-  const routes: Routes = [ 
+const routes: Routes = [
   { path: '', component: SlideshowComponent },
-  { path: 'imprint', component: ImprintComponent }
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'data-protection', component: DataProtectionComponent }
 ];
 
 @NgModule({
