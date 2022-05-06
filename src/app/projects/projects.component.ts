5df20205d8d21c1666b5d0158262af83a27357e6
Valentina-developer <valentina.kuxhausen@web.de>
2022-05-06 14:09:35 +0200

    router to imprint

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,11 +10,14 @@ export class ProjectsComponent implements OnInit {
   projects = [{
     'name': 'Join',
     'image': 'assets/img/join.png',
-    'description': 'Task Management System'
+    'description': 'Task Management System',
+    'link': 'url("http://valentina-kuxhausen.developerakademie.net/m10_gruppenarbeit_join_ftp/gruppenarbeit-174/addtask.html")'
     },{
     'name': 'Ring of Fire',
     'image': 'assets/img/ring_of_fire.png',
-    'description': 'Task Management System'
+    'description': 'Multiple players card game',
+    'link': 'href=http://valentina-kuxhausen.developerakademie.net/m10_gruppenarbeit_join_ftp/gruppenarbeit-174/addtask.html'
+
     }
   ]
 
@@ -23,7 +26,6 @@ export class ProjectsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  //NGFOR IN HTML - SHOW PROJECTS
 
 
 
