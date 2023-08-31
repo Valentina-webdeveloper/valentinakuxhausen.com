90adf79fd39f36a4eb25eb1992db5a323845327f
ValentinaKuxhausen <valentina.kuxhausen@web.de>
2023-08-31 12:54:46 +0200

    css adjustment

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -3,60 +3,65 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-skills',
   templateUrl: './my-skills.component.html',
-  styleUrls: ['./my-skills.component.scss']
+  styleUrls: ['./my-skills.component.scss'],
 })
 export class MySkillsComponent implements OnInit {
   skills: any = [
     {
       icon: 'js.png',
-      name: 'JavaScript'
+      name: 'JavaScript',
     },
     {
       icon: 'angular.png',
-      name: 'Angular'
+      name: 'Angular',
     },
     {
       icon: 'html.png',
-      name: 'HTML5'
+      name: 'HTML5',
     },
     {
       icon: 'sass.png',
-      name: 'SASS / SCSS'
+      name: 'SASS / SCSS',
     },
     {
       icon: 'scrum.png',
-      name: 'Scrum'
+      name: 'Scrum',
     },
     {
       icon: 'Git.png',
-      name: 'Git'
+      name: 'Git',
     },
     {
       icon: 'rest-api.png',
-      name: 'Rest API'
+      name: 'Rest API',
     },
     {
       icon: 'firebase.png',
-      name: 'Firebase'
+      name: 'Firebase',
     },
     {
       icon: 'azure.png',
-      name: 'MS Azure'
+      name: 'MS Azure',
     },
     {
       icon: 'confluence.png',
-      name: 'Confluence'
+      name: 'Confluence',
     },
     {
       icon: 'test-lab.png',
-      name: 'Test Automation'
-    }
-
+      name: 'Test Automation',
+    },
+    {
+      icon: 'intellij-icon.png',
+      name: 'IntelliJ',
+    },
+    {
+      icon: 'vscode-icon.png',
+      name: 'VS Code',
+    },
   ];
-  
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
